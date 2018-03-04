a2f8304e7708e1042a875ee39288abb451d0366f
Alexandre Resende <aar6947@gmail.com>
2018-03-03 22:00:03 -0300

    refactor: Change routes to use destructuring

--- api/routes/income.routes.js
+++ api/routes/income.routes.js
@@ -1,17 +1,24 @@
+
 const express = require('express');
 const router = express.Router();
 
-const incomeController = require('../controllers/income.controller');
+const {
+  getAllIncomes,
+  insertIncomes,
+  updateIncomes,
+  removeIncomes,
+  getIncomesCurrentMonth,
+} = require('../controllers/income.controller');
 
-router.get('/:userId/incomes', incomeController.getAllIncomes);
+router.get('/:userId/incomes', getAllIncomes);
 
-router.post('/:userId/incomes', incomeController.insertIncomes);
+router.post('/:userId/incomes', insertIncomes);
 
-router.put('/:userId/incomes/:incomeId', incomeController.updateIncomes);
+router.put('/:userId/incomes/:incomeId', updateIncomes);
 
-router.delete('/:userId/incomes/:incomeId', incomeController.removeIncomes);
+router.delete('/:userId/incomes/:incomeId', removeIncomes);
 
-router.get('/:userId/monthlyIncome', incomeController.getIncomesCurrentMonth);
+router.get('/:userId/monthlyIncome', getIncomesCurrentMonth);
 
 
 module.exports = router;
